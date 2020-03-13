9be343ec7e49b885f8c3aff6725fa3fa025b5e23
ritamxy <ritamxy@gmail.com>
2020-03-13 21:00:01 +1100

    Introducing <OrderSummaryList /> total

--- src/components/OrderSummaryList/OrderSummaryList.jsx
+++ src/components/OrderSummaryList/OrderSummaryList.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Section from "../Section";
 import Item from "./components/Item";
+import styled from "styled-components";
+
+const Total = styled.div`
+  text-align: right;
+  font-size: 18px;
+  font-weight: 300;
+`;
+
+const getTotal = (size, toppings) => {
+  const value = toppings.reduce(
+    (prevValue, { price }) => prevValue + price,
+    size.price
+  );
+
+  return value.toFixed(2);
+};
 
 const OrderSummaryList = ({ selectedSize, chosenToppings }) => (
   <Section title="Order summary">
     <Item size={selectedSize} toppings={chosenToppings} />
     <hr />
+    <Total>{`Total: $${getTotal(selectedSize, chosenToppings)}`}</Total>
   </Section>
 );
 
